4fadc0fd2b00ae73d9be055ec76aaecdbe020f79
Abdul Khan <akhan17@outlook.com>
2020-04-02 19:02:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
-  def initialize(name,breed)
+  def initialize(name,breed = "Mutt")
     @name = name
     @breed = breed
   end
 
   attr_accessor :name
-  
-  def breed(val = "Mutt")
+
+  def breed=(val)
     @breed = val
   end
-  
+
   def breed
     @breed
   end
